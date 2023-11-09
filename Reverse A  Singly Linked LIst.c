3f1fe8a395f8499f7cad21612d2bf889aec30b72
Papai Mistry <103406706+papaimistry@users.noreply.github.com>
2023-11-09 12:12:19 +0530

    Add files via upload

---
+++ Reverse A  Singly Linked LIst.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+
+struct Node {
+	int data;
+	struct Node *next;
+}*temp,*temp2;
+
+int main(){
+	struct Node *head = NULL;
+  head = (struct Node *)malloc(sizeof(struct Node));
+  head->data = 1;
+  head->next = (struct Node *)malloc(sizeof(struct Node));
+  head->next->data = 2;
+  head->next->next = (struct Node *)malloc(sizeof(struct Node));
+  head->next->next->data = 3;
+  head->next->next->next = NULL;
+  
+  reverse(&head);
+  
+  while(head != NULL){
+  	printf("[%d]->",head->data);
+  	head=head->next;
+  }
+	return 0;
+}
+
+void reverse(struct Node** head_ref){
+	while(*head_ref != NULL){
+		temp2=(*head_ref)->next;
+		(*head_ref)->next=temp;
+		temp=(*head_ref);
+		(*head_ref)=temp2;
+	}
+	(*head_ref)=temp;
+}
